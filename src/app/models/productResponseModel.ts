e6234514232afeed2010302a34da9632033e0372
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-04-19 15:47:41 +0200

    generated product service and refactored

--- src/app/models/productResponseModel.ts
+++ src/app/models/productResponseModel.ts
@@ -1,5 +1,6 @@
 import { Product } from "./product";
-import { ResponseModel } from "./ResponseModel";
+import { ResponseModel } from "./responseModel";
+
 
 export interface ProductResponseModel extends ResponseModel{
     data:Product[],
