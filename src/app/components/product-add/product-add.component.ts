c056b0882b9b54880eb3e35743a465f3e22ea3d0
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-07-13 17:43:36 +0200

    refactoring

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,30 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,FormControl,Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
-  styleUrls: ['./product-add.component.css']
+  styleUrls: ['./product-add.component.css'],
 })
 export class ProductAddComponent implements OnInit {
-
   productAddForm: FormGroup;
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+    private toastrService:ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.createProductAddForm();
   }
 
-  createProductAddForm(){
+  createProductAddForm() {
     this.productAddForm = this.formBuilder.group({
-      productName:["",Validators.required],
-      unitPrice:["", Validators.required],
-      unitsInStock:["", Validators.required],
-      categoryId:["",Validators.required]
-    })
+      productName: ['', Validators.required],
+      unitPrice: ['', Validators.required],
+      unitsInStock: ['', Validators.required],
+      categoryId: ['', Validators.required],
+    });
   }
-  
-  add(){
+
+  add() {
+    if (this.productAddForm.valid) {
+      let productModel = Object.assign({}, this.productAddForm.value);
+      this.productService.add(productModel).subscribe(response=>{
+        console.log(response)
+        this.toastrService.success(response.message, "Success")
+      },responseError=>{
+        console.log(responseError.error)
+        this.toastrService.error(responseError.error)
+      })
+    } else {
+      this.toastrService.error("Your attention form is missing!!")
+    }
 
   }
 }
